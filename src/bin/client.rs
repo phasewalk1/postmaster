73ebcd880810f8aabec244e287df6f6e4a486b3f
phaswalk1 <staticanne@skiff.com>
2022-11-22 14:26:30 -0800

    init template

---
+++ src/bin/client.rs
@@ -0,0 +1,6 @@
+use mess::pb as prostgen;
+use prostgen::messenger::messenger_client::MessengerClient;
+use prostgen::messenger::{MsgRequest, MsgResponse};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {}
---
+++ src/bin/server.rs
@@ -0,0 +1,5 @@
+use mess::pb as prostgen;
+use prostgen::messenger::messenger_server::{Messenger, MessengerServer};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {}
